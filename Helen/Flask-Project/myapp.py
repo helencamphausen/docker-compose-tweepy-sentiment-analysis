0b49c6b10c63ec9dd41c24cf2ca1422fdd9fabe3
helen camphausen <helencamphausen.helen.camphausen@gmail.com>
2019-11-11 11:32:20 +0100

    deleted helen folder

--- Helen/Flask-Project/myapp.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return '<h1>Hello from Flask this is Helens first Flask App</h1>'
-
-app.run(host='0.0.0.0', debug=True)
